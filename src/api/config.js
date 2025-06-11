b4c368de45438acab7b25d04db3c6c69ed1a3f49
zenanvibe <zenanvibe@gmail.com>
2025-06-11 18:01:04 +0530

    AWS API Integrated

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 // api/config.s
 // const API = "http://localhost:5000/api/v2/";
-const API = "https://swotle-backend-5ns5.onrender.com/api/v2/";
+const API = "https://swotle-backend-1-s8md.onrender.com/api/v2/";
 // api
 export default API;
