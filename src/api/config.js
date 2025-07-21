b7eca59913bb4c5c6ff442ff9f54883bd1e4940b
zenanvibe <zenanvibe@gmail.com>
2025-07-21 14:40:40 +0530

    BE port change

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 // api/config.s
 // const API = "http://localhost:5000/api/v2/";
-const API = "https://dashboardapi.swotle.com:5500/api/v2/";
+const API = "https://dashboardapi.swotle.com:5501/api/v2/";
 // const API = "https://swotle-backend-1-s8md.onrender.com/api/v2/";
 // api
 export default API;
